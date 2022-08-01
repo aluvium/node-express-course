59315816ff337baf8e787753238ca88837c05c40
aluvium <aluvium@protonmail.com>
2022-08-01 09:10:33 +0000

    add dynamic var

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ const mockUserData=[
 ]
 
 app.get('/users', function(req,res){
- 	res.json({
- 	 	success: true,
- 	 	message: 'Succesfully got users',
- 	 	users: mockUserData
- 	})
+    res.json({
+        success: true,
+        message: 'Succesfully got users',
+        users: mockUserData
+    })
 })
 app.get('/users/:id', function(req,res){
-        console.log(req.params.id)
-	res.json({
-		success: true,
-		message: 'Got one user',
-		users: req.params.id
-	})
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'Got one user',
+        users: req.params.id
+    })
 })
 
 app.listen(8000,function() {
-  console.log("Server is running..")
+    console.log("Server is running..")
 })
