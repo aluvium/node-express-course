7c1954e2a9394ab2e9f2c0e72deb29e8249f2c01
aluvium <aluvium@protonmail.com>
2022-08-01 15:31:33 +0000

    post route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+ 
 const mockUserData=[
 {name:'alice'},
 {name:'kate'},
