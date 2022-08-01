a1e28c36259499e5108eaa38415c2db9b66743ce
aluvium <aluvium@protonmail.com>
2022-08-01 08:47:54 +0000

    create first rquest / respond function --> /users

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const mockUserData=[
+{name:'Alice'},
+{name:'Kate'},
+{name:'Sonya'}
+]
+
+app.get('/users', function(req,res){
+ 	res.json({
+ 	 	success: true,
+ 	 	message: 'Succesfully got users',
+ 	 	users: mockUserData
+ 	})
+})
 
 app.listen(8000,function() {
   console.log("Server is running..")
